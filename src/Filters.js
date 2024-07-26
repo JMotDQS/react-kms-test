1e03d7ebace7c0ba74e3560f0a19b9449adbedf9
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-07-26 16:08:06 -0400

    more styling

--- src/Filters.js
+++ src/Filters.js
@@ -4,9 +4,9 @@ import './css/filters.css';
 const Filters = () => {
 	return (
 		<div className="filters">
-			<button>Button 1</button>
-			<button>Button 2</button>
-			<button>Button 3</button>
+			<button>Registered</button>
+			<button>Keys</button>
+			<button>Yard</button>
 			<button>Button 4</button>
 		</div>
 	)
