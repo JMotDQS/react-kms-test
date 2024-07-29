fe81a2cf74ac1a329790ecc4889852bf7ff6db53
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-07-29 10:19:19 -0400

    adding display class of invisible

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,7 +4,7 @@ import './css/dashboard.css';
 const Dashboard = () => {
 	return (
 		<div className="dashboard-container">
-			<div className="card custom-report-card">Custom</div>
+			<div className="card custom-report-card invisible">Custom</div>
 			<div className="card">Registered</div>
 			<div className="card">Keys</div>
 			<div className="card">Yard</div>
